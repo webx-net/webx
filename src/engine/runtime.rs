bd9a8829eff80186ab8c6ad72fd2021a00ec4e9d
William Rågstad <william.ragstad@gmail.com>
2023-10-15 16:30:12 +0200

    Format

--- src/engine/runtime.rs
+++ src/engine/runtime.rs
@@ -1,10 +1,23 @@
-use std::{sync::mpsc::Receiver, net::{SocketAddr, TcpListener, TcpStream}, io::Write, collections::HashMap};
+use std::{
+    collections::HashMap,
+    io::Write,
+    net::{SocketAddr, TcpListener, TcpStream},
+    sync::mpsc::Receiver,
+};
 
-use http::{Method, Response, request};
+use http::{request, Method, Response};
 
-use crate::{file::webx::{WXModule, WXModulePath, WXUrlPath, WXBody, WXRoute, WXBodyType}, runner::WXMode, reporting::{debug::info, warning::warning, error::error_code}, analysis::routes::verify_model_routes};
+use crate::{
+    analysis::routes::verify_model_routes,
+    file::webx::{WXBody, WXBodyType, WXModule, WXModulePath, WXRoute, WXUrlPath},
+    reporting::{debug::info, error::error_code, warning::warning},
+    runner::WXMode,
+};
 
-use super::http::{parse_request_tcp, serialize_response, Responses::{ok_html, self}};
+use super::http::{
+    parse_request_tcp, serialize_response,
+    Responses::{self, ok_html},
+};
 
 /// A runtime error.
 pub struct WXRuntimeError {
@@ -24,7 +37,7 @@ pub struct WXRTRoute {
 
 impl WXRTRoute {
     /// Execute the route and return a HTTP response.
-    /// 
+    ///
     /// ## Note
     /// This function will **not** check if the route is valid.
     fn execute(&self) -> Result<Response<String>, WXRuntimeError> {
@@ -32,18 +45,18 @@ impl WXRTRoute {
             Ok(match body.body_type {
                 WXBodyType::TS => {
                     todo!("(Runtime) TS body type is not supported yet");
-                },
+                }
                 WXBodyType::TSX => {
                     // Assume that the body is valid JSX without dynamic content.
                     // That is, the body is a static HTML page or fragment.
                     ok_html(body.body.clone())
-                },
+                }
             })
         } else {
             // TODO: Add support for handlers as well.
             Err(WXRuntimeError {
                 code: 500,
-                message: "Route body is empty".into()
+                message: "Route body is empty".into(),
             })
         }
     }
@@ -73,12 +86,14 @@ impl WXRouteMap {
         let mut route_map: WXRouteMapInner = HashMap::new();
         // Insert all routes into each method map category.
         for ((route, path), _) in routes.unwrap().iter() {
-            let method_map = route_map.entry(route.method.clone()).or_insert(HashMap::new());
+            let method_map = route_map
+                .entry(route.method.clone())
+                .or_insert(HashMap::new());
             method_map.insert(path.clone(), Self::compile_route(route)?);
         }
         Ok(WXRouteMap(route_map))
     }
-    
+
     /// Compile a parsed route into a runtime route.
     fn compile_route(route: &WXRoute) -> Result<WXRTRoute, WXRuntimeError> {
         let body = route.body.clone();
@@ -87,7 +102,7 @@ impl WXRouteMap {
 
     /// Get a route from the route map.
     /// This function will return `None` if the route does not exist.
-    /// 
+    ///
     /// ## Note
     /// This function will **not** check for duplicate routes.
     /// This is done in the `analyse_module_routes` function.
@@ -100,7 +115,9 @@ impl WXRouteMap {
         // Go through all routes and try to match the path.
         for (route_path, route) in routes {
             dbg!("Checking", route_path, route, route_path.matches(path));
-            if route_path.matches(path) { return Some((route_path, route)); }
+            if route_path.matches(path) {
+                return Some((route_path, route));
+            }
         }
         None
     }
@@ -127,12 +144,12 @@ impl WXRuntime {
             source_modules: Vec::new(),
             routes: WXRouteMap::new(),
             messages: rx,
-            mode
+            mode,
         }
     }
 
     /// Load a list of modules into the runtime.
-    /// 
+    ///
     /// ## Note
     /// This function will **not** recompile the route map.
     /// To recompile the route map, either:
@@ -143,11 +160,11 @@ impl WXRuntime {
     }
 
     /// Tries to recompile all loaded modules at once and replace the runtime route map.
-    /// 
+    ///
     /// ## Note
     /// This way we can get cross-module route analysis, which is required for detecting
     /// duplicate routes and other global errors.
-    /// 
+    ///
     /// ## Error
     /// This function will **throw and error** if the route map cannot be compiled
     /// from the current source modules, and will **not** replace the current route map.
@@ -155,7 +172,7 @@ impl WXRuntime {
     fn recompile_routes(&mut self) {
         match WXRouteMap::from_modules(&self.source_modules) {
             Ok(routes) => self.routes = routes,
-            Err(err) => error_code(format!("(Runtime) {}", err.message), err.code)
+            Err(err) => error_code(format!("(Runtime) {}", err.message), err.code),
         }
         if self.mode == WXMode::Dev {
             dbg!(&self.routes); // TODO: Remove this line
@@ -196,18 +213,27 @@ impl WXRuntime {
         while let Ok(msg) = self.messages.try_recv() {
             match msg {
                 WXRuntimeMessage::NewModule(module) => {
-                    info(self.mode, &format!("(Runtime) New module: {}", module.path.module_name()));
+                    info(
+                        self.mode,
+                        &format!("(Runtime) New module: {}", module.path.module_name()),
+                    );
                     self.source_modules.push(module);
                     self.recompile_routes();
-                },
+                }
                 WXRuntimeMessage::SwapModule(path, module) => {
-                    info(self.mode, &format!("(Runtime) Reloaded module: {}", module.path.module_name()));
+                    info(
+                        self.mode,
+                        &format!("(Runtime) Reloaded module: {}", module.path.module_name()),
+                    );
                     self.source_modules.retain(|m| m.path != path);
                     self.source_modules.push(module);
                     self.recompile_routes();
-                },
+                }
                 WXRuntimeMessage::RemoveModule(path) => {
-                    info(self.mode, &format!("(Runtime) Removed module: {}", path.module_name()));
+                    info(
+                        self.mode,
+                        &format!("(Runtime) Removed module: {}", path.module_name()),
+                    );
                     self.source_modules.retain(|m| m.path != path);
                     self.recompile_routes();
                 }
@@ -216,7 +242,7 @@ impl WXRuntime {
     }
 
     /// Listen for incoming requests.
-    /// 
+    ///
     /// ## Blocking
     /// This function is **non-blocking** and will return immediately depending on the
     /// value of `listener.set_nonblocking` in the `run` function.
@@ -229,11 +255,22 @@ impl WXRuntime {
     /// Handle an incoming request.
     fn handle_request(&self, mut stream: TcpStream, addr: SocketAddr) {
         if let Some(request) = parse_request_tcp::<()>(&stream) {
-            info(self.mode, &format!("(Runtime) Request from: {}\n{:#?}", addr, &request));
+            info(
+                self.mode,
+                &format!("(Runtime) Request from: {}\n{:#?}", addr, &request),
+            );
             // Match the request to a route.
             let url = request.uri().path();
             if let Some((path, route)) = self.routes.resolve(request.method(), url) {
-                info(self.mode, &format!("(Runtime) Route: {} {}, matches '{}'", request.method(), path, url));
+                info(
+                    self.mode,
+                    &format!(
+                        "(Runtime) Route: {} {}, matches '{}'",
+                        request.method(),
+                        path,
+                        url
+                    ),
+                );
                 let response = match route.execute() {
                     Ok(response) => response,
                     Err(err) => {
@@ -241,12 +278,25 @@ impl WXRuntime {
                         Responses::internal_server_error_default_webx(self.mode, err.message)
                     }
                 };
-                stream.write(serialize_response(&response).as_bytes()).unwrap();
-                info(self.mode, &format!("(Runtime) Response to: {}\n{:#?}", addr, &response));
-                info(self.mode, &format!("(Runtime) Response body:\n{}", &response.body()));
+                stream
+                    .write(serialize_response(&response).as_bytes())
+                    .unwrap();
+                info(
+                    self.mode,
+                    &format!("(Runtime) Response to: {}\n{:#?}", addr, &response),
+                );
+                info(
+                    self.mode,
+                    &format!("(Runtime) Response body:\n{}", &response.body()),
+                );
             } else {
                 warning(format!("(Runtime) Route not found: {}", url));
-                stream.write(serialize_response(&Responses::not_found_default_webx(self.mode)).as_bytes()).unwrap();
+                stream
+                    .write(
+                        serialize_response(&Responses::not_found_default_webx(self.mode))
+                            .as_bytes(),
+                    )
+                    .unwrap();
             }
             stream.flush().unwrap();
         } else {
