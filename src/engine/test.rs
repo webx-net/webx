6a38a247356d20f8d747214daec7d99e24659fa8
William Rågstad <william.ragstad@gmail.com>
2023-10-15 15:05:38 +0200

    Make test non-blocking

--- src/engine/test.rs
+++ src/engine/test.rs
@@ -2,10 +2,22 @@
 mod tests {
     use std::path::Path;
 
-    use crate::runner::{run, WXMode};
+    use crate::{runner::{WXMode, get_project_config_file_path}, file::project::{load_project_config, load_modules}, analysis::{dependencies::analyse_module_deps, routes::analyse_module_routes}, engine::runtime::WXRuntime};
 
     #[test]
     fn test_example_todo() {
-        run(&Path::new("examples/todo"), WXMode::Dev);
+        let mode = WXMode::Dev;
+        let root = &Path::new("examples/todo");
+        let config = load_project_config(&get_project_config_file_path(root)).unwrap();
+        let source_root = root.join(&config.src);
+        let webx_modules = load_modules(&source_root);
+        analyse_module_deps(&webx_modules);
+        analyse_module_routes(&webx_modules);
+        let (_, dummy_rx) = std::sync::mpsc::channel();
+        let mut runtime = WXRuntime::new(dummy_rx, mode);
+        runtime.load_modules(webx_modules);
+        std::thread::spawn(move || runtime.run());
+        std::thread::sleep(std::time::Duration::from_secs(10));
+        std::process::exit(0);
     }
 }
