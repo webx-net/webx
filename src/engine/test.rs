3b586427b0ee2e9c6ac3a3a351249b0e361d17a2
William Rågstad <william.ragstad@gmail.com>
2023-10-21 18:09:15 +0200

    Fix runtime in test

--- src/engine/test.rs
+++ src/engine/test.rs
@@ -7,16 +7,18 @@ mod tests {
     #[test]
     fn test_example_todo() {
         let mode = WXMode::Dev(DebugLevel::Max);
-        let root = &Path::new("examples/todo");
+        let root = Path::new("examples/todo");
         let config = load_project_config(&get_project_config_file_path(root)).unwrap();
         let source_root = root.join(&config.src);
         let webx_modules = load_modules(&source_root);
         analyse_module_deps(&webx_modules);
         analyse_module_routes(&webx_modules);
         let (_, dummy_rx) = std::sync::mpsc::channel();
-        let mut runtime = WXRuntime::new(dummy_rx, mode, WXRuntimeInfo::new(root));
-        runtime.load_modules(webx_modules);
-        std::thread::spawn(move || runtime.run());
+        std::thread::spawn(move || {
+            let mut runtime = WXRuntime::new(dummy_rx, mode, WXRuntimeInfo::new(root));
+            runtime.load_modules(webx_modules);
+            runtime.run();
+        });
         std::thread::sleep(std::time::Duration::from_secs(10));
         std::process::exit(0);
     }
