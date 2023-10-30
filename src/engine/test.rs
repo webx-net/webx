348c97389443d6b2ae0065d2f524ab858a50b4ce
William Rågstad <william.ragstad@gmail.com>
2023-10-30 20:25:04 +0100

    Update test.rs

--- src/engine/test.rs
+++ src/engine/test.rs
@@ -4,6 +4,14 @@ mod tests {
 
     use crate::{runner::{WXMode, get_project_config_file_path, DebugLevel}, file::project::{load_project_config, load_modules}, analysis::{dependencies::analyse_module_deps, routes::analyse_module_routes}, engine::runtime::{WXRuntime, WXRuntimeInfo}};
 
+    /// Kill the runtime after `TIMEOUT` seconds.
+    /// This is useful for debugging the runtime
+    /// and when the tests are run in a CI/CD environment
+    /// where the runtime should **NOT** run forever.
+    static KILL_AFTER_TIMEOUT: bool = true;
+    /// Kill the runtime after `TIMEOUT` seconds.
+    static TIMEOUT: u64 = 10;
+
     #[test]
     fn test_example_todo() {
         let mode = WXMode::Dev(DebugLevel::Max);
@@ -14,12 +22,18 @@ mod tests {
         analyse_module_deps(&webx_modules);
         analyse_module_routes(&webx_modules);
         let (_, dummy_rx) = std::sync::mpsc::channel();
-        std::thread::spawn(move || {
+        if KILL_AFTER_TIMEOUT {
+            std::thread::spawn(move || {
+                let mut runtime = WXRuntime::new(dummy_rx, mode, WXRuntimeInfo::new(root));
+                runtime.load_modules(webx_modules);
+                runtime.run();
+            });
+            std::thread::sleep(std::time::Duration::from_secs(TIMEOUT));
+            std::process::exit(0);
+        } else {
             let mut runtime = WXRuntime::new(dummy_rx, mode, WXRuntimeInfo::new(root));
             runtime.load_modules(webx_modules);
             runtime.run();
-        });
-        std::thread::sleep(std::time::Duration::from_secs(10));
-        std::process::exit(0);
+        }
     }
 }
