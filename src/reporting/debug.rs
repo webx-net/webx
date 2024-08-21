16f54ddd3aba780d0caf789e9b8a63fd0a9dbba2
WilliamRagstad <william.ragstad@gmail.com>
2024-08-21 22:39:54 +0200

    Fix warning and info formats

--- src/reporting/debug.rs
+++ src/reporting/debug.rs
@@ -7,7 +7,7 @@ pub fn info(mode: WXMode, text: &str) {
     if mode.is_dev() && mode.debug_level().is_medium() {
         let now = Local::now();
         let time = now.format("%d/%m %H:%M:%S");
-        let prefix = format!("[Info {}]", time);
+        let prefix = format!("Info (T{})", time);
         println!("{}: {}", prefix.bright_cyan(), text);
     }
 }
